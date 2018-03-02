f132038c5c74644eb2575e56ad3be057d010f1ab
c-nguyen <c-nguyen@tribalmedia.com>
2018-03-02 10:58:16 +0900

    [MODIFY] folder structure

---
+++ FMPhotoPicker/FMPhotoPicker/source/ImageEditor/FMImageEditor.swift
@@ -0,0 +1,13 @@
+//
+//  FMImageEditor.swift
+//  FMPhotoPicker
+//
+//  Created by c-nguyen on 2018/03/02.
+//  Copyright © 2018 Tribal Media House. All rights reserved.
+//
+
+import Foundation
+
+struct FMImageEditor {
+    
+}
